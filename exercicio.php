c98a152af90758a3be87962801276719af03181d
Christiano <mafradesouza.cs@gmail.com>
2020-08-24 10:09:48 -0300

    login

--- exercicio.php
+++ exercicio.php
@@ -1,5 +1,22 @@
+<?php
+session_start();
+
+if($_COOKIE['usuario']){
+    $_SESSION['usuario'] = $_COOKIE['usuario'];
+}
+
+if(!$_SESSION['usuario']){
+    header('Location: login.php');
+}
+
+if (!$_SESSION['usuario']) {
+    header('Location: login.php');
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
@@ -7,20 +24,22 @@
     <link rel="stylesheet" href="recursos/css/exercicio.css">
     <title>Exercício</title>
 </head>
+
 <body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
-            class="verde">Sem formatação</a>
-        <a href="index.php" class="vermelho">Voltar</a>
+        <span class="usuario">Usuário: <?= $_SESSION['usuario'] ?></span>
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem formatação</a>
+        <a href="index.php" class="azul">Voltar</a>
+        <a href="logout.php" class="vermelho">Sair</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
             <?php
-                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
             ?>
         </div>
     </main>
@@ -28,4 +47,5 @@
         COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
 </body>
+
 </html>
